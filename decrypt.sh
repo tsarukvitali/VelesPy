c02b1a1dedcbf58fe4f426035369c064fce52633
TSV <tilsit_89@mail.ru>
2019-10-04 17:52:53 +0300

    fix variable

--- decrypt.sh
+++ decrypt.sh
@@ -13,20 +13,20 @@ case $decrypt in
         read sdata
         echo -n "Enter end date: "
         read edata
-        mydir=$(mktemp)
-        mydir2=$(mktemp)
-        mydir3=$(mktemp)
-        egrep -r 'BOX 0 l|BOX 1 l|LCDM: e|BOX 0 - u|BOX 1 - u|bill end status|LCDM box 1 blocked|LCDM box 0 blocked|answer: Timeout|sensor status|LCDM: read timeout|Counting error|Motor stop status|SOL sensor|Pickup error|Over reject status' log/ | sed -r 's!(^[^\(]+\()!(!g' >> $mydir 
-        sort --output=$mydir2 $mydir
+        findvariable=$(mktemp)
+        sortstrings=$(mktemp)
+        delstrings=$(mktemp)
+        egrep -r 'BOX 0 l|BOX 1 l|LCDM: e|BOX 0 - u|BOX 1 - u|bill end status|LCDM box 1 blocked|LCDM box 0 blocked|answer: Timeout|sensor status|LCDM: read timeout|Counting error|Motor stop status|SOL sensor|Pickup error|Over reject status' log/ | sed -r 's!(^[^\(]+\()!(!g' >> $findvariable 
+        sort --output=$sortstrings $findvariable
         sdata="(${sdata}"
         edata="(${edata}"
         # echo $sdata
-        snumber=$(grep -n "$sdata" $mydir2 | cut -f1 -d:) 
+        snumber=$(grep -n "$sdata" $sortstrings | cut -f1 -d:) 
         #number=$(($number-1))
         #echo $snumber
-        sed '1,'$snumber'd' $mydir2 >> $mydir3
-        enumber=$(grep -n "$edata" $mydir3 | cut -f1 -d:) 
-        sed ''$enumber',$d' $mydir3 >> inkass.txt
+        sed '1,'$snumber'd' $sortstrings >> $delstrings
+        enumber=$(grep -n "$edata" $delstrings | cut -f1 -d:) 
+        sed ''$enumber',$d' $delstrings >> inkass.txt
         for file in ./log/*
         do
 	        grep -A 17 'BOX 0 l' $file >> inkass.txt
@@ -51,6 +51,8 @@ case $decrypt in
         exit 1
     ;;
 esac
+
+
 
 
 
