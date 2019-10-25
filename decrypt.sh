1a0b9919b03627ba791695cd523512f9319d773a
TSV <tilsit_89@mail.ru>
2019-10-25 12:29:15 +0300

    add scp log and decrypt

--- decrypt.sh
+++ decrypt.sh
@@ -31,9 +31,9 @@ inkass_action() {
     sortstrings=$(mktemp)
     snumberf=$(mktemp)
     delstrings=$(mktemp)
-    egrep -r 'BOX [0-1] l|LCDM: e|BOX [0-1] - u|LCDM: command .44|bill end status|LCDM box [0-1] blocked|answer: Timeout|sensor status|LCDM: read timeout|Counting error|Motor stop status|SOL sensor|Pickup error|Over reject status|Reject tray is not recognized' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
+    egrep -sr 'BOX [0-1] l|LCDM: e|BOX [0-1] - u|LCDM: command .44|bill end status|LCDM box [0-1] blocked|answer: Timeout|sensor status|LCDM: read timeout|Counting error|Motor stop status|SOL sensor|Pickup error|Over reject status|Reject tray is not recognized' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable" 
     sort --output="$sortstrings" "$findvariable"
-    snumber=$(grep -n "BOX [0-1] l" "$sortstrings" | cut -f1 -d:)
+    snumber=$(grep -n "BOX [0-1] l" "$sortstrings" | cut -f1 -d:) 
     echo "$snumber" >> "$snumberf"
     numberoflines=$(wc -l "$snumberf" | cut -f1 -d\ )
     if [ "$numberoflines" == "2" ]; then
@@ -48,8 +48,8 @@ inkass_action() {
     fi
     for file in ./log/*
     do
-        grep -A 17 'BOX [0-1] l' "$file" >> $inkass_file
-        grep -B 20 'unload from   cashbox' "$file" >> $inkass_file
+        grep -sA 17 'BOX [0-1] l' "$file" >> $inkass_file 
+        grep -sB 20 'unload from   cashbox' "$file" >> $inkass_file 
     done
 }
 
@@ -63,7 +63,7 @@ keno_action() {
     
     findvariable=$(mktemp)
     delstrings=$(mktemp)
-    egrep -r ' BD: | BL | BMA: |BEGIN' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
+    egrep -sr ' BD: | BL | BMA: |BEGIN' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
     echo -n "Enter start date(YYYY-MM-DD HH:MM): "
     read sdata
     echo -n "Enter end date(YYYY-MM-DD HH:MM): "
@@ -91,14 +91,14 @@ bur_action() {
     
     findvariable=$(mktemp)
     sortstrings=$(mktemp)
-    egrep -r 'Escrow command|Stacked complete|BillAcceptor|CCNet: error read answer|initialize billacceptor on port| BD: 1| BD: 2| BD: 5' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
+    egrep -sr 'Escrow command|Stacked complete|BillAcceptor|CCNet: error read answer|initialize billacceptor on port| BD: 1| BD: 2| BD: 5' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
     sort --output="$sortstrings" "$findvariable"
     echo "$burlegend" >> $bur_file
     echo >> $bur_file
     cat "$sortstrings" >> $bur_file
     for file in ./log/*
     do
-        grep -A 41 'BD: EncAcceptorBtn ' "$file" >> $bur_file
+        grep -sA 41 'BD: EncAcceptorBtn ' "$file" >> $bur_file
     done
 }
 
@@ -112,7 +112,7 @@ balance_action() {
     
     findvariable=$(mktemp)
     delstrings=$(mktemp)
-    egrep -r 'BillAcceptor|total spin|SpinTotal|BEGIN| BL | RB: | RW: | TW: | Balance |LUA:|enter double|opened|LCDM: e|BOX [0-1] - u|SSP: dd |paycenter|exit double' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
+    egrep -sr 'BillAcceptor|total spin|SpinTotal|BEGIN| BL | RB: | RW: | TW: | Balance |LUA:|enter double|opened|LCDM: e|BOX [0-1] - u|SSP: dd |paycenter|exit double' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
     echo -n "Enter start date(YYYY-MM-DD HH:MM): "
     read sdata
     echo -n "Enter end date(YYYY-MM-DD HH:MM): "
@@ -142,19 +142,19 @@ bill_action() {
     
     findvariable=$(mktemp)
     sortstrings=$(mktemp)
-    egrep -r 'Escrow command|Stacked command|BillAcceptor| BD: 2| BD: 1| BD: 2| BD: 5|Transport|jammed status|CCTALK: error read answer|billacceptor' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
+    egrep -sr 'Escrow command|Stacked command|BillAcceptor| BD: 2| BD: 1| BD: 2| BD: 5|Transport|jammed status|CCTALK: error read answer|billacceptor' log/ | sed -r 's!(^[^\(]+\()!(!g' >> "$findvariable"
     sort --output="$sortstrings" "$findvariable"
     echo "$billlegend" >> $bill_file
     echo >> $bill_file
     cat "$sortstrings" >> $bill_file
     for file in ./log/*
     do
-        grep -A 45 'BD: EncAcceptorBtn ' "$file" >> $bill_file
+        grep -sA 45 'BD: EncAcceptorBtn ' "$file" >> $bill_file
     done
 }
 
 
-
+case_action(){
 case $1 in
     1 | --inkass) inkass_action
         code inkass.txt
@@ -171,20 +171,33 @@ case $1 in
     5 | --keno) keno_action
         code keno.txt
     ;;
-    6 | --cctalk) egrep -r 'CCTALK| cctalk' log/ | sed -r 's!(^[^\(]+\()!(!g' >>cctalk.txt
+    6 | --cctalk) egrep -sr 'CCTALK| cctalk' log/ | sed -r 's!(^[^\(]+\()!(!g' >>cctalk.txt
     ;;
-    7 | --lcdm) egrep -r 'LCDM' log/ | sed -r 's!(^[^\(]+\()!(!g' >>LCDM.txt
+    7 | --lcdm) egrep -sr 'LCDM' log/ | sed -r 's!(^[^\(]+\()!(!g' >>LCDM.txt
     ;;
-    8 | --ccnet) egrep -r 'CCNET' log/ | sed -r 's!(^[^\(]+\()!(!g' >>CCNET.txt
+    8 | --ccnet) egrep -sr 'CCNET' log/ | sed -r 's!(^[^\(]+\()!(!g' >>CCNET.txt
     ;;
     -h | --help) display_help
     ;;
-    9) echo "Yes, Vlad has nine fingers:)"
+    9 ) echo "Yes, Vlad has nine fingers:)"
     ;;
     *) echo "$1 is not an option. Please use $0 --help"
         exit 1
     ;;
 esac
+}
+
+if [ -n "$2" ]
+then
+mkdir -p log
+scp -i "key.pem" -r root@54.216.225.233:/raid/data/telemetry/"$2"/*/platform* ./log/
+python3 unlog.py
+case_action "$1" 
+zip "$2".zip ./log/*.txt
+else
+case_action "$1" 
+zip log.zip ./log/*.txt
+fi
 
 
 
