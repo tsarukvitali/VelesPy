59ee11421d22a77c2392e3ad2028f0d605dff3c3
TSV <tilsit_89@mail.ru>
2019-11-05 18:42:00 +0300

    modify zip

--- moduleconnect.py
+++ moduleconnect.py
@@ -9,17 +9,16 @@ path = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(path + '/raw_log/'):
     os.makedirs(path + '/raw_log/')
 keyfile =str(path + '/key/key.pem')
-outpath = path + '/raw_log/log.gz'
-print(keyfile)
-command = 'gzip -c ' + remotepath + ' >> /tmp/log.gz'
-print(command)
+outpath = path + '/raw_log/logs.tar.gz'
+command = 'tar -czvf /tmp/logs.tar.gz ' + remotepath 
+command2 = 'rm /tmp/logs.tar.gz'
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('54.93.125.153', username='ubuntu', key_filename=keyfile)
-
 stdin, stdout, stderr = ssh.exec_command(command)
 sftp = ssh.open_sftp()
-sftp.get('/tmp/log.gz', outpath )
+sftp.get('/tmp/logs.tar.gz', outpath )
+stdin, stdout, stderr = ssh.exec_command(command2)
 ssh.close()
 
 
